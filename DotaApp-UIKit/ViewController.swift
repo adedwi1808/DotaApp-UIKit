fe3b729bc230f9a4f5755113f506399ad644d061
adedwi1808 <adedwip1808@gmail.com>
2023-01-16 21:19:10 +0700

    Initial Commit

---
+++ DotaApp-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DotaApp-UIKit
+//
+//  Created by Ade Dwi Prayitno on 16/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
